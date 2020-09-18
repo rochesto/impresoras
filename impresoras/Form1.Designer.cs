49161cb8873e65dc67f8128a0add9ddc164837e7
Roberto Espinosa <rochesto@hotmail.com>
2020-09-18 15:28:18 +0200

    0.1

--- impresoras/Form1.Designer.cs
+++ impresoras/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
+
 
 namespace impresoras
 {
@@ -32,21 +32,24 @@ namespace impresoras
         /// </summary>
         private void InitializeComponent()
         {
-            this.initilizeVariables();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.marca = new System.Windows.Forms.Label();
             this.modelo = new System.Windows.Forms.Label();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonInstall = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
-            this.comboBox1.Items.AddRange(this.marcas.getNameFolders());
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(39, 45);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(201, 24);
             this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
@@ -55,6 +58,7 @@ namespace impresoras
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(200, 24);
             this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // marca
             // 
@@ -74,39 +78,56 @@ namespace impresoras
             this.modelo.TabIndex = 3;
             this.modelo.Text = "Modelo";
             // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 331);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(447, 107);
+            this.textBox1.TabIndex = 4;
+            // 
+            // buttonInstall
+            // 
+            this.buttonInstall.Location = new System.Drawing.Point(549, 45);
+            this.buttonInstall.Name = "buttonInstall";
+            this.buttonInstall.Size = new System.Drawing.Size(98, 23);
+            this.buttonInstall.TabIndex = 5;
+            this.buttonInstall.Text = "Instalar";
+            this.buttonInstall.UseVisualStyleBackColor = true;
+            this.buttonInstall.Click += new System.EventHandler(this.buttonInstall_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonInstall);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.modelo);
             this.Controls.Add(this.marca);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
-        private void initilizeVariables()
-        {
-            String marcasPath = Path.Combine(Directory.GetCurrentDirectory(), @"maquinas");
-            this.marcas = new Printers(marcasPath);
-
-            //String modeloPath = Path.Combine(marcasPath, this.comboBox1.Text);
-            //this.modelos = new Printers(modeloPath);
-        }
-
         #endregion
-        private Printers marcas;
-        private Printers modelos;
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label marca;
         private System.Windows.Forms.Label modelo;
+        private EventLog eventLog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonInstall;
     }
 }
 
--- impresoras/Form1.cs
+++ impresoras/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,48 @@ namespace impresoras
 {
     public partial class Form1 : Form
     {
+        private Printers marcas;
+        private String marcasPath = "";
+        private Printers modelos;
+        private String modelosPath = "";
+
         public Form1()
         {
             InitializeComponent();
+            this.initilizeVariables();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void initilizeVariables()
+        {
+            this.marcasPath = Path.Combine(Directory.GetCurrentDirectory(), @"maquinas");
+            this.marcas = new Printers(marcasPath);
+            this.comboBox1.Items.AddRange(this.marcas.getNameFolders());
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                this.modelosPath = Path.Combine(marcasPath, this.comboBox1.Text);
+                this.modelos = new Printers(this.modelosPath);
+                this.comboBox2.Items.AddRange(this.modelos.getNameFolders());
+            }
+            catch(Exception exc)
+            {
+                this.textBox1.AppendText(exc.Message);
+            }
+            
+        }
+
+        private void buttonInstall_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
